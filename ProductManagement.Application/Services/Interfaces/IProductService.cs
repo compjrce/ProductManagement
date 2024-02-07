7810df124ef724bdee7c4bdb77a92cea4ec8a840
Francisco Silva <compjrce@gmail.com>
2024-02-06 22:46:00 -0300

    Add product service with crud methods

---
+++ ProductManagement.Application/Services/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Application.Services.Interfaces;
+
+public interface IProductService
+{
+    Task<Product> GetById();
+
+    Task<List<Product>> GetAll();
+
+    Task Create(Product product);
+
+    Task Update(Guid id, Product product);
+
+    Task Delete(Guid id);
+
+}
---
+++ ProductManagement.Application/Services/ProductService.cs
@@ -0,0 +1,32 @@
+using ProductManagement.Application.Services.Interfaces;
+using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Application.Services;
+
+public class ProductService : IProductService
+{
+    public Task Create(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Guid id, Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
