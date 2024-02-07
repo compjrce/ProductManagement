1672701b7f080f9c265c366717ef8e95ac4c3e8a
Francisco Silva <compjrce@gmail.com>
2024-02-07 09:36:34 -0300

    Add dbcontext to dependency injection

--- ProductManagement.Infrastructure/InfrastructureModule.cs
+++ ProductManagement.Infrastructure/InfrastructureModule.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using ProductManagement.Domain.Repositories;
+using ProductManagement.Infrastructure.Persistence;
 using ProductManagement.Infrastructure.Persistence.Repositories;
 
 namespace ProductManagement.Infrastructure;
@@ -8,7 +9,23 @@ public static class InfrastructureModule
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services
+            .AddRepositories()
+            .AddDbContext();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
         services.AddScoped<IProductServiceRepository, ProductServiceRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<ProductDbContext>();
 
         return services;
     }
